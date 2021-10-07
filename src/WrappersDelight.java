c266158909deb3a23abc47291952ebf52274681f
joshua-fasalojo <fasalif@ssfs.org>
2021-10-07 09:00:40 -0400

    Methods with Parameters Exercises

--- src/WrappersDelight.java
+++ src/WrappersDelight.java
@@ -4,4 +4,25 @@ public class WrappersDelight {
     // When finished, uncomment the WrappersDelight.main(null) line in the
     // test file to make sure your program is correct.
 
+    public static void main(String[] args) {
+          int num1 = 14;
+          Integer num2 = (8);
+          double num3 = 2.5;
+          double num4 = num2 + num3;
+          num2 = 5;
+        num3 = num1 + num2 + num4;
+        num2 + num4 = num1;
+
+        System.out.print ("14");
+        System.out.println("8");
+        System.out.println("5");
+
+
+
+
+
+
+    }
+
+
 }
