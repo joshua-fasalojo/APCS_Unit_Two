9af9daf961ec6bd3f8fa9ecef36a8a5527af650d
joshua-fasalojo <fasalif@ssfs.org>
2021-10-16 08:09:37 -0400

    Methods with Parameters Exercises.

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -10,6 +10,11 @@ public class EmailGenerator<main> {
         String firstInitial =  name.substring(0,1);
         int indexof = name.indexOf(" ") + 1;
 
+        double max;
+        double min;
+        int randomNumber = (int) (Math.random()*(max-min)) +min;
+
+
 
 
     }
