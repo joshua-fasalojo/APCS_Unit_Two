a89116452ad4e948b708db2f84b43b8f4ad760d5
joshua-fasalojo <fasalif@ssfs.org>
2021-10-22 09:48:56 -0400

    Methods with Parameters Exercises.

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -1,22 +1,29 @@
 public class EmailGenerator<main> {
     public static void main(String[] args) {
-        String myName = "Brian";
-    makeUserName(myName);
-    //System.out.print("jfasalojo ");
+        String newString = "george fox";
+        String username = makeUserName(newString);
+        System.out.println(username);
+        String address = "icloud.com";
+        String emailaddress = makeEmail(username, address);
+        System.out.println(emailaddress);
+
     }
 
-    private static void makeUserName(String name) {
+    public static String makeUserName(String name) {
         // name = Josh Fasalojo -> jfasalojo
         String firstInitial =  name.substring(0,1);
-        int indexof = name.indexOf(" ") + 1;
+        int space = name.indexOf(" ");
+        String lastName = name.substring(space+1);
 
         int randomNumber = (int) (Math.random()*(99-10 + 1)) +10;
+        return firstInitial + lastName + randomNumber;
 
 
 
     }
-    public static void makeEmail(String name){
-        String makeUsername = jfasalojo;
+
+    public static String makeEmail(String name, String address){
+        return name + "@" + address;
     }
 
 
