f476cc235f1fc05f1ffd005a31d8502ef7067615
joshua-fasalojo <fasalif@ssfs.org>
2021-12-03 19:36:35 -0500

    Why are there so many zeros?

--- src/PointsAndRectangles.java
+++ src/PointsAndRectangles.java
@@ -8,22 +8,32 @@ public class PointsAndRectangles
 
     public static void main(String[] args)
     {
-        Scanner kb = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         // create print statements and get input for the x and y values.
-        int x = kb.nextInt();
-        int y = kb.nextInt();
+        System.out.println("Enter the x cordinate: ");
+        int x = input.nextInt();
+        System.out.println("Enter the y cordinate: ");
+        int y = input.nextInt();
         //Instantiate a Point object p. Hint: you will need the keyword "new"
-        Point p = new Point(x,y);
-
+        Point p = new Point();
         // create print statements and get input for the length and width
+        System.out.println("Enter the length: ");
+        int length = input.nextInt();
+        System.out.println("Enter the width: ");
+        int width = input.nextInt();
         //System.out.print(0,0);
         //Instantiate a Dimension object d.
         Dimension d = new Dimension();
+
         //Instantiate a Rectangle object r.
         Rectangle r = new Rectangle();
 
+
         // Print each object p, d, and r on a separate line.
         System.out.println(p);
+        System.out.println(d);
+        System.out.println(r);
+
         //System.out.println(d);
         //System.out.println(r);
     }
