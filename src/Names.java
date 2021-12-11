497f815ad472f9e03614eb56705aa19cb1d71fa7
joshua-fasalojo <fasalif@ssfs.org>
2021-12-11 13:44:23 -0500

    Names

--- src/Names.java
+++ src/Names.java
@@ -10,7 +10,7 @@ public class Names {
         String firstName = input.nextLine();;
         System.out.println("Enter your last name: ");
         String lastName = input.nextLine();
-        String fullname = firstName + lastName;
+        String fullname = firstName + " "+ lastName;
         System.out.println(fullname);
         System.out.println(fullname.length());
         System.out.print(firstName.substring(0,1));
@@ -18,12 +18,20 @@ public class Names {
         String lastname = new String();
         int var1 = firstName.compareTo(lastname);
         System.out.println(var1);
-        String str = "Fasalojo";
-        String str2 = "Joshua";
-        char ch = 'a';
-        int PosofA = str.indexOf(str,ch);
-        int mid1 = 3;
-        int mid2 = 7;
+        System.out.println(lastname.indexOf('a'));
+        System.out.println(firstName.indexOf('o'));
+        char ch1 = firstName.charAt(0);
+        char ch2 = firstName.charAt(1);
+        char ch3 = firstName.charAt(2);
+        char ch4 = lastName.charAt(5);
+        char ch5 = lastName.charAt(6);
+        char ch6 = lastName.charAt(7);
+        char ch7 = lastName.charAt(8);
+        char ch8 = lastName.charAt(0);
+        char ch9 = lastName.charAt(1);
+        char ch10  = lastName.charAt(2);
+        char ch11 = lastName.charAt(3);
+        System.out.print(ch1 + ch2 + ch3 + ch4 + ch5 + ch6 + ch7 + " " + ch8 + ch9 + ch10 + ch11);
 
 
 
