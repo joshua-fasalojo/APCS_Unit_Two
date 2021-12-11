d194a0252e76f3df1f9c8a27ee7f40f48f1efa6e
joshua-fasalojo <fasalif@ssfs.org>
2021-12-11 16:50:07 -0500

    tempera

--- src/TemperatureDifference.java
+++ src/TemperatureDifference.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class TemperatureDifference {
+
+
+
     /**
      * This method will calculate the difference in two temperatures, regardless of which one is larger.
      * @param temp1 - an int temperature value
@@ -8,18 +11,24 @@ public class TemperatureDifference {
      * @return the absolute value of the difference between the two temps. Make sure to change the 0
      */
     public static int calculateDifference(int temp1, int temp2) {
+       int finalTemperature = (int) (Math.absExact((0)));
 
         return 0;
     }
 
     public static void main(String[] args) {
         // Scanner object to get input
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Get the two temperatures from the user as input
-        //(int) Math.random();
+        System.out.print("Enter the first temperature: ");
+        int temp1 = input.nextInt();
+        System.out.print("Enter the second temperature: ");
+        int temp2 = input.nextInt();
+        System.out.println( (int) Math.absExact(temp1));
         // Call the calculateDifference method and save the result in a variable.
-
+        int finalTemperature = (int) (Math.absExact(input.nextInt()));
         // Print out the information to the user
+        System.out.println("The temperature changed " + finalTemperature + " degrees" );
     }
 }
